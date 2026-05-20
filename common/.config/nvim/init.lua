192732a2bfc9fe702e90ac04fd5e99ac20ac8a15
Johnny Wu <karasaya@gmail.com>
2026-05-20 09:53:51 +0800

    add ssh clipboard support for neovim

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -106,6 +106,22 @@ if vim.g.neovide then
   -- })
 end
 
+-- ssh settings
+if vim.env.SSH_TTY ~= nil then
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+      ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+  }
+  vim.opt.clipboard = 'unnamedplus'
+end
+
 -- DELME: {
 local function OSX()
   return is_mac
